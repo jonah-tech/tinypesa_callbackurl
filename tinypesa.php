1c6b6a69265b68d1de054c3345b89a1e546c94d1
jonah-tech <77667318+jonah-tech@users.noreply.github.com>
2022-02-21 17:31:02 +0300

    Update tinypesa.php

--- tinypesa.php
+++ tinypesa.php
@@ -1,7 +1,7 @@
 <?php
 $amount = '1'; //Amount to transact 
 $phonenuber = '07xxxxxxx'; // Phone number paying
-$Account_no = 'UMESKIA SOFTWARES'; // Enter account number optional
+$Account_no = 'JONAH-TECH SOLUTIONS'; // Enter account number optional
 $url = 'https://tinypesa.com/api/v1/express/initialize';
 $data = array(
     'amount' => $amount,
@@ -10,7 +10,7 @@ $data = array(
 );
 $headers = array(
     'Content-Type: application/x-www-form-urlencoded',
-    'ApiKey: RLyaVXpqEx9' // Replace with your api key
+    'ApiKey: QLyaVXqwEx4' // Replace with your api key
  );
 $info = http_build_query($data);
 
@@ -28,7 +28,7 @@ if($msg_resp ->success == 'true'){
 }
 echo 'Transaction Completed  successfully. Now redirecting...';
   
-  header("Location: https://placid.co.ke/");
+  header("Location: https://example.co.ke/");
 
 ?>
 
